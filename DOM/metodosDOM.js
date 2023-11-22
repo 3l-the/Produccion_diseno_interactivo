9aff363657f848a516271ea231b730d662b44927
Eduardo Mulhia <emulhiad@gmail.com>
2023-11-21 19:20:34 -0600

    Metodos DOM

---
+++ DOM/metodosDOM.js
@@ -0,0 +1,28 @@
+let encabezado = document.getElementById("encabezado");
+let mensajes = document.getElementsByClassName("mensaje");
+let parrafos = document.getElementsByTagName("p");
+let primerParrafo = document.querySelector("p");
+//encabezado.textContent = "Encabezado"; 
+//encabezado.innerText = "Encabezado Inner"; Estos dos cambian el texto
+
+//encabezado.style.color="Cyan"; Modifica estilos
+
+//encabezado.classList.add("active");
+//encabezado.classList.remove("active"); Agrega o elimina clases
+
+//let nuevoDiv = document.createElement("div");
+//nuevoDiv.textContent = "Soy un nuevo Div";
+//document.body.appendChild(nuevoDiv);
+
+//let referencia = document.getElementById("encabezado");
+//document.body.insertBefore(nuevoDiv, referencia);
+
+//encabezado.remove();
+
+/*function onClick(){
+    console.log("Boton presionado");
+}
+let boton = document.getElementById("miBoton");
+boton.addEventListener("click", onClick);
+
+boton.removeEventListener("click", onClick);*/
